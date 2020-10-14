5d44fe9723a5a66f15c2d1a2f22e695df6fa7e66
Mihaly Banfi <mihaly.banfi@tappointment.com>
2020-10-14 11:20:09 +0200

    Add Navbar, Button, HeroSection components

---
+++ landing-ite/src/components/HeroSection.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "./Button";
+import './HeroSection.css';
+import '../App.css'
+import video from '../assets/videos/DJI_0379.MP4'
+
+export default function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src={video} autoPlay loop muted />
+      <h1> ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large">
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large">
+          WATCH TRAILER<i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- landing-ite/src/components/Navbar.js
+++ landing-ite/src/components/Navbar.js
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
+import "./Navbar.css";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const showButton = () => window.innerWidth <= 960 ? setButton(false) : setButton(true);
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navabr-logo">
-            SKLTR48 <i className="fas fa-skull" />
+          <Link to="/landing-ite" className="navbar-logo" onClick={closeMobileMenu}>
+            SKLTR48 &nbsp; <i className="fas fa-skull" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              <Link to="/landing-ite" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
@@ -41,13 +51,15 @@ export default function Navbar() {
             </li>
             <li className="nav-item">
               <Link
-                to="/signup"
-                className="nav-links"
-                onClick={closeMobileMenu}>
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+                >
                 Sign Up
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
